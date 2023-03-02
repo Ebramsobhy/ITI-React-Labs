1f78703b0c76d791e73a0cf267f77d24d032c2b8
Ebram Sobhy Isaac <116510911+Ebramsobhy@users.noreply.github.com>
2023-03-02 21:08:38 +0200

    Delete Lab 1 directory

--- Lab 1/src/App.js
+++
@@ -1,9 +0,0 @@
-import Counter from './views/counter'
-
-function App() {
-  return (
-      <Counter/>  
-  );
-}
-
-export default App;
--- Lab 1/src/components/counter/Count.jsx
+++
@@ -1,8 +0,0 @@
-const Count = ({count}) => {
-    return <h1 className="count text-primary">{count}</h1>
-};
-
-export default Count;
-
-
-
